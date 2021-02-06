daf0e9925757d5a259743490a4bf31a1c5ca4f76
Muhammad Kamran <mkamran1967@gmail.com>
2021-02-06 15:15:13 -0800

    Authorization on the frontend

--- app/lib/useAuth.tsx
+++ app/lib/useAuth.tsx
@@ -16,3 +16,38 @@ type AuthProps = {
 };
 
 const AuthContext = createContext<Partial<AuthProps>>({});
+
+function useProvideAuth() {
+  const client = useApolloClient();
+  const router = useRouter();
+
+  const [error, setError] = useState('');
+  const { data } = useCurrentUserQuery({
+    // network-only - So user data/authentication is always done via server not cache.
+    fetchPolicy: 'network-only',
+    // ignore errors so the app doesn't crash, remove for debugging
+    errorPolicy: 'ignore',
+  });
+
+  const user = data && data.currentUser;
+
+  // Signing In and Signing Up
+  const [signInMutation] = useSignInMutation();
+  const [signUpMutation] = useSignUpMutation();
+
+  const signIn = async (email, password) => {
+    try {
+      const { data } = await signInMutation({ variables: { email, password } });
+      if (data.login.token && data.login.user) {
+        sessionStorage.setItem('token', data.login.token);
+        client.resetStore().then(() => {
+          router.push('/');
+        });
+      } else {
+        setError('Invalid Login');
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+}
